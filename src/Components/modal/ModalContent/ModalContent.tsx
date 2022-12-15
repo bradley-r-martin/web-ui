5d38e6c7c91336980162943005375d7c94243875
Bradley Martin <bradley.r.martin@me.com>
2022-12-15 16:41:43 +1100

    feat(modal): modal remove loading animation

--- src/Components/modal/ModalContent/ModalContent.tsx
+++ src/Components/modal/ModalContent/ModalContent.tsx
@@ -8,7 +8,7 @@ const ModalContent: FunctionComponent<ModalContentProps> = (props) => {
   const { children, className, loading, ...native } = props
   return (
     <div className={`bg-gray-50 relative overflow-hidden  ${className}`} {...native}>
-      <NProgress isAnimating={loading}>
+      {/* <NProgress isAnimating={loading}>
         {({ animationDuration, isFinished, progress }) => (
           <div
             style={{
@@ -54,7 +54,8 @@ const ModalContent: FunctionComponent<ModalContentProps> = (props) => {
         >
           {children}
         </motion.div>
-      </div>
+      </div> */}
+      {children}
     </div>
   )
 }
